56574618e54b9bac7f4f7ea6f781e5277d0f9491
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-04-15 09:36:46 +0200

    Medikamente leer

--- views/Medikamente.py
+++ views/Medikamente.py
@@ -1,23 +1,5 @@
-#Muss bearbeitet werden!
-
-
 import streamlit as st
 
-st.title('BMI Verlauf')
+st.title('Medikamente')
 
-data_df = st.session_state['data_df']
-if data_df.empty:
-    st.info('Keine BMI Daten vorhanden. Berechnen Sie Ihren BMI auf der Startseite.')
-    st.stop()
 
-# Weight over time
-st.line_chart(data=data_df.set_index('timestamp')['weight'])
-st.caption('Gewicht über Zeit (kg)')
-
-# Height over time
-st.line_chart(data=data_df.set_index('timestamp')['height'])
-st.caption('Größe über Zeit (m)')
-
-# BMI over time
-st.line_chart(data=data_df.set_index('timestamp')['bmi'])
-st.caption('BMI über Zeit')
