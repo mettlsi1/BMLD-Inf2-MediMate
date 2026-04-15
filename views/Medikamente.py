35c1bbf9143475aef79bc663f6597710338f8691
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-04-15 11:36:04 +0200

    Medikamente hinzufügen Button

--- views/Medikamente.py
+++ views/Medikamente.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.title('Medikamente')
 
-
+col1, col2, col3 = st.columns(3)
+    with col1:
+        if st.button("Neues Medikament hinzufügen"):
+            st.session_state.page = "add_medikament"
+    with col2:
+        if st.button("Medikamenten-Auflistung"):
+            st.session_state.page = "list_medikamente"
