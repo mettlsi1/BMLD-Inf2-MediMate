0a3f67690e12e21c9e370feb441cee5d335529ac
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-04-15 09:34:12 +0200

    homepage und Medikamente page

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import pandas as pd 
 import streamlit as st
+
+st.set_page_config(page_title="MediMate", page_icon=":material/home:")
+
+pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
+pg_second = st.Page("views/Medikamente.py", title="Medikamente", icon="☢️")
+
+pg = st.navigation([pg_home, pg_second])
+pg.run()
