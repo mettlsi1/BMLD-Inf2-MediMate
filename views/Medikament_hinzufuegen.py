875bc3f29a37fc0f92260a472cc7c8872bc7a1ae
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-04-21 09:52:52 +0200

    Kommentare

--- views/Medikament_hinzufuegen.py
+++ views/Medikament_hinzufuegen.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 
 st.title('Medikament hinzufügen')
-
+# Initialisiere die Medikamentenliste in der Session, falls sie noch nicht existiert
 if "medikamente" not in st.session_state:
     st.session_state.medikamente = []
 
 st.markdown("Bitte fülle die folgenden Felder aus, um ein neues Medikament zu speichern.")
-
+# Erstelle ein Formular für die Medikamenteneingabe
 with st.form("add_medication_form"):
     name = st.text_input("Medikamentenname")
     col_dosis, col_einheit = st.columns([3, 2])
@@ -24,7 +24,7 @@ with st.form("add_medication_form"):
     weiteres = st.selectbox("Weiteres", ["--", "Vor dem Essen", "Mit dem Essen", "Nach dem Essen"])
     
     submitted = st.form_submit_button("Hinzufügen")
-    
+    # Überprüfe die Eingaben und füge das Medikament zur Liste hinzu
     if submitted:
         if name.strip() and dosis > 0:
             dosis_str = f"{dosis} {einheit}"
