c9a4e2e5a3d06460f4666396fa06f536d2f65636
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-05-01 10:14:44 +0200

    Rückängig machen der letzten Löschung

--- views/Kalender.py
+++ views/Kalender.py
@@ -96,6 +96,10 @@ if st.session_state.medikamente:
             day_label = "🟡 Morgen"
         else:
             day_label = calendar.day_name[current_date.weekday()]
+        
+        # Starte den graublau gefärbten Container
+        with st.container():
+            st.markdown(f"<h4>{day_label} – {current_date.strftime('%d.%m.%Y')}</h4>", unsafe_allow_html=True)
             
             # Zeige die Medikamente für diese Tageszeiten in 3 Spalten
             times_of_day = ["Morgen", "Mittag", "Abend"]
