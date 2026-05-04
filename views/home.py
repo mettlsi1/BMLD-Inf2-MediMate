20663be6bf8ca233d4fe456f1cb0826fa602eb95
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-05-04 20:46:29 +0200

    Button Kalender auf Homepage

--- views/home.py
+++ views/home.py
@@ -9,6 +9,9 @@ st.info("""Bitte konsultieren Sie einen Arzt für eine vollständige Beurteilung
 if st.button("Meine Medikamente"):
     st.switch_page("views/Medikamente.py")
 
+if st.button("Kalender"):
+    st.switch_page("views/Kalender.py")
+
 st.write("Diese App wurde von den folgenden Personen im Rahmen des Moduls 'BMLD Informatik 2' an der ZHAW entwickelt:")
 st.write("- Jessica Schmid (schmij30@students.zhaw.ch)")
 st.write("- Valeria Schönyan (schoeva1@students.zhaw.ch)")
