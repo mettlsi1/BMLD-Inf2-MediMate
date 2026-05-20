4312d9928555ae5116bd7924e8040e90b033fb7d
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-05-20 11:37:03 +0200

    Achsenbeschriftung

--- views/Blutdruckgrafik.py
+++ views/Blutdruckgrafik.py
@@ -1,10 +1,9 @@
-import streamlit as st
+import altair as alt
 import pandas as pd
+import streamlit as st
 from functions.Blutdruckeingabe_functions import initialize_blutdruck_state
 
 st.title("Blutdruckgrafik")
-
-# Initialisiere die Blutdruckdaten (falls noch nicht geschehen)
 initialize_blutdruck_state(st.session_state.data_manager)
 
 if "blutdruck" not in st.session_state or not st.session_state.blutdruck:
@@ -15,8 +14,18 @@ else:
         bp_df["Datum"] = pd.to_datetime(bp_df["Datum"])
         bp_df = bp_df.sort_values("Datum")
 
-        st.line_chart(
-            bp_df.set_index("Datum")[["Systolisch", "Diastolisch", "PWS"]]
+        chart = alt.Chart(bp_df).transform_fold(
+            ["Systolisch", "Diastolisch", "PWS"],
+            as_=["Messwert", "Wert"]
+        ).mark_line(point=True).encode(
+            x=alt.X(
+                "Datum:T",
+                axis=alt.Axis(title="Datum und Uhrzeit", format="%d.%m %H:%M", labelAngle=-45)
+            ),
+            y=alt.Y("Wert:Q", title="Messwert"),
+            color="Messwert:N"
         )
+
+        st.altair_chart(chart, use_container_width=True)
     else:
         st.info("Noch keine Blutdruckwerte gespeichert.")
